4bac3177453f911fef8483eb22106768af9b536b
bricksky <skydh1214@naver.com>
2025-05-20 16:14:01 +0900

    테이블 칼럼 중복 오류 해결

--- src/main/java/org/sopt/domain/Post.java
+++ src/main/java/org/sopt/domain/Post.java
@@ -13,9 +13,6 @@ public class Post {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Column(nullable = false)
-    private Long userId;
-
     @Column(nullable = false, length = 30)
     private String title;
 
