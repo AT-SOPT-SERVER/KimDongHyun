ee50d335a80a2f70cc97ca1b817368dc68c0ec2c
bricksky <skydh1214@naver.com>
2025-05-20 16:47:36 +0900

    FEAT: 댓글 기능 Repository 구현

---
+++ src/main/java/org/sopt/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package org.sopt.repository;
+
+import org.sopt.domain.Post;
+import org.sopt.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostOrderByCreatedAtAsc(Post post);
+}
